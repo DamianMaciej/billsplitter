817e3adbe497229255863f84b1d40b5dab16e0ff
Damian Maciejewski <damian.maciej0@gmail.com>
2023-04-26 16:48:02 +0200

    Minor problems fix

--- main.js
+++ main.js
@@ -6,39 +6,44 @@ const errorInfo = document.querySelector(`.error`);
 const cost = document.querySelector(`.cost`);
 const endInfo = document.querySelector(`.cost-info`);
 
+const currency = document.querySelector(`.currency`);
+const currencyInfo = document.querySelector(`.currency-info`);
 
 
 const process = () => {
-
+    
     const countBill = () => {
         errorInfo.textContent = ``;
-
+        
         const newPrice = parseFloat(costToPay.value);
         const newPeople = parseInt(people.value);
         const newTip = parseFloat(tip.value);
-
+        
         if (newTip == 0) {
             const sum = newPrice / newPeople;
             endInfo.style.display = `block`;
-
+            
             cost.textContent = sum.toFixed(2);
-        } else {
+        }else {
             const sum = (newPrice + (newPrice * newTip)) / newPeople;
             endInfo.style.display = `block`;
-
+            
             cost.textContent = sum.toFixed(2);
         }
+
+        currencyInfo.textContent = currency.value;
+        
     }
 
-    if (costToPay.value != `` && people.value != ``) {
+    if (costToPay.value != `` && people.value != ``){
         countBill()
 
-    } else {
-        errorInfo.textContent = `ERROR: use correct data and fill in the blanks in the form.`;
+    }else {
+        errorInfo.textContent = `ERROR: use correct data and fill in the blanks in the form!`;
         endInfo.style.display = `none`;
     }
 
-
+    
 }
 
 btnCount.addEventListener(`click`, process);
